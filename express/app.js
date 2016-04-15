1d97f43551b238fbd287cd2baad67d47696b1a39
dcampagna <campagna@esteco.com>
2016-04-15 10:04:31 +0200

    express app

---
+++ express/app.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var app = express();
+
+var productMap = {
+    'a': 100,
+    's': 1200,
+    'd': 323
+};
+
+app.get('/catalog', function (req, res) {
+    res.send(productMap);
+});
+
+app.use(express.static('dist'));
+
+app.listen(3000, function() {
+    console.log('Point of Sale listening on port 3000!');
+});
